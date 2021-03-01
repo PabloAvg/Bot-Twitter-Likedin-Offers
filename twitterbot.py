baf3c4ad4ebf20771e8a0059e4fa05ce53dc4fd1
Pablo Alvarez <pabloalvarezgarcia99@gmail.com>
2021-03-01 12:07:29 +0100

    Update twitterbot.py
    Funcional falta soporte de servidor

--- twitterbot.py
+++ twitterbot.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+import requests
 import tweepy
 import time
 
@@ -17,14 +19,79 @@ for status in tweepy.Cursor(twitter_API.user_timeline).items():
     except:
         pass
 
-##Lista de seguidores
-followers = twitter_API.followers()
-def mention_followers():
-    for follower in followers:
-        print(follower.screen_name)
-        twitter_API.update_status('Hello 2 @' + follower.screen_name)
+web = requests.get('https://es.linkedin.com/jobs/search?keywords=inform%C3%A1tica&location=Madrid%2C%2BComunidad%2Bde%2BMadrid%2C%2BEspa%C3%B1a&trk=homepage-jobseeker_jobs-search-bar_search-submit&f_TP=1&sortBy=DD&f_PP=103374081&redirect=false&position=1&pageNum=0')
 
-##Llamamos a la funcion en un bucle infinito:
+#print(web.content)
+
+soup = BeautifulSoup(web.content, 'html.parser')
+
+#print(soup.prettify())
+
+"""
+for job in soup.find_all('a'):
+    if job.get('class') == ['result-card__full-card-link']:
+        print(job.get('href'))
+
+for job in soup.find_all('a'):
+    if job.get('class') == ['result-card__full-card-link']:
+        j = requests.get(job.get('href'))
+        s = BeautifulSoup(j.content, 'html.parser')
+        print(job.get_text())
+        print(job.get('href'))
+        for des in s.find_all('h3'):
+            if des.get('class') == ['job-criteria__subheader']:
+                print(des.get_text())
+        for des in s.find_all('span'):
+            if des.get('class') == ['job-criteria__text', 'job-criteria__text--criteria']:
+                print(des.get_text())                
+"""
+
+todayJobs = []
+
+for job in soup.find_all('a'):
+    if job.get('class') == ['result-card__full-card-link']:
+        oneJob = []
+        j = requests.get(job.get('href'))
+        s = BeautifulSoup(j.content, 'html.parser')
+        oneJob.append(job.get_text())
+        oneJob.append(job.get('href'))
+        for des in s.find_all('h3'):
+            if des.get('class') == ['job-criteria__subheader']:
+                oneJob.append(des.get_text())
+        for des in s.find_all('span'):
+            if des.get('class') == ['job-criteria__text', 'job-criteria__text--criteria']:
+                oneJob.append(des.get_text())
+        todayJobs.append(oneJob)
+
+# print(todayJobs)
+
+def tweetJobs():
+    tweet = ""
+    for job in todayJobs:
+        tweet += 'Nombre: '+ job[0] + "\n"
+        tweet += job[2] + ': '+ job[6] + "\n"
+        tweet +=job[3] + ': '+ job[7] + "\n"
+        tweet += job[4] + ': ' + job[8] + "\n"
+        tweet += job[5] + ': '+ job[9] + '...' + "\n"
+        tweet += 'Link: '+ job[1]
+        print(tweet)
+        print('\n\n\n')
+        twitter_API.update_status(tweet)
+        tweet = ""
+
 while True:
-   mention_followers()
+   tweetJobs()
    time.sleep(3600)
+
+
+"""
+for job in todayJobs:
+    print('Nombre:', job[0])
+    print(job[2] + ':', job[6])
+    print(job[3] + ':', job[7])
+    print(job[4] + ':', job[8])
+    print(job[5] + ':', job[9] + '...')
+    print('Link:', job[1])
+    #print('\n\n\n')
+    #twitter_API.update_status()
+"""
